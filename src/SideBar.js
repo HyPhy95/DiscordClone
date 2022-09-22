7f5342c5cab50f77929e698bfd65c1c893074a6d
Firas <HyPhy95@users.noreply.github.com>
2022-09-22 16:30:41 +0800

    first commit

---
+++ src/SideBar.js
@@ -0,0 +1,31 @@
+import { BsPlus, BsFillLightningFill, BsGearFill, BsMoonStarsFill } from 'react-icons/bs';
+import { FaFire } from 'react-icons/fa';
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+                    
+        <SideBarIcon icon={<FaFire size="28" />} />
+        <Divider />
+        <SideBarIcon icon={<BsPlus size="32" />} />
+        <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+        <SideBarIcon icon={<BsMoonStarsFill size="20" />} />
+        <Divider />
+        <SideBarIcon icon={<BsGearFill size="22" />} />
+    </div>
+  );
+};
+
+const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
+  <div className="sidebar-icon group">
+    {icon}
+    <span class="sidebar-tooltip group-hover:scale-100">
+      {text}
+    </span>
+  </div>
+);
+
+
+const Divider = () => <hr className="sidebar-hr" />;
+
+export default SideBar;
